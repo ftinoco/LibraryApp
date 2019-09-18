dc62ba6fe7e53d68fe96411e207ef4f443105cf5
Fernando Tinoco <ftinoco_>
2019-09-17 19:58:56 -0600

    WebApi initial configuration

--- LibraryApp.API/App_Start/WebApiConfig.cs
+++ LibraryApp.API/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace LibraryApp.API
 {
@@ -10,6 +12,7 @@ namespace LibraryApp.API
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +22,15 @@ namespace LibraryApp.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Remove XML serialization
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            // Add json formatting
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            // Define how to handle loop reference
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            // Define contract resolver
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
