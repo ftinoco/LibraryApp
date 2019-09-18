6ad055ca4c7d843953751290f723519947324c80
Fernando Tinoco <ftinoco_>
2019-09-17 20:37:59 -0600

    Mapping database with entity framework and link with MetadataType

---
+++ LibraryApp.API/Models/PartialClasses.cs
@@ -0,0 +1,29 @@
+﻿using LibraryApp.API.Models.ApiModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApp.API.Models.Context
+{
+    [MetadataType(typeof(AuthorApiModel))]
+    public partial class Autor
+    {
+
+    }
+
+    [MetadataType(typeof(StudentApiModel))]
+    public partial class Estudiante
+    {
+
+    }
+
+    [MetadataType(typeof(BookApiModel))]
+    public partial class Libro
+    {
+
+    }
+
+    [MetadataType(typeof(LoanApiModel))]
+    public partial class Prestamo
+    {
+
+    }
+}
