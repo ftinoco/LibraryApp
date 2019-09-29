26dc56ff1f5e5b40f8a5e2c4ffb7a60ae1f391ee
ftinoco <fernandotinoco_45@hotmail.com>
2019-09-29 16:56:38 -0600

    It was completed Students controller funcionalities

--- LibraryApp.API/Controllers/StudentsController.cs
+++ LibraryApp.API/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryApp.API.Models.Context;
+using LibraryApp.Common;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -9,10 +10,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using LibraryApp.API.Models.Context;
-using LibraryApp.Common;
-using LibraryApp.API.Models.ApiModel;
-using Newtonsoft.Json;
 
 namespace LibraryApp.API.Controllers
 {
@@ -20,10 +17,9 @@ namespace LibraryApp.API.Controllers
     {
         private LibraryEntities db = new LibraryEntities();
 
-        // GET: api/Students
         public async Task<HttpResponseMessage> GetStudents([FromUri] DataFilter filter)
         {
-            List<Estudiante> lst = null; 
+            List<Estudiante> lst = null;
 
             if (!string.IsNullOrWhiteSpace(filter.filter))
             {
@@ -32,11 +28,11 @@ namespace LibraryApp.API.Controllers
                 lst = await db.Estudiante.Where(x => x.Nombre.ToUpper().Trim().Contains(strFilter) ||
                                                  x.Direccion.ToUpper().Trim().Contains(strFilter) ||
                                                  x.CI.ToUpper().Trim().Contains(strFilter) ||
-                                                 x.Carrera.ToUpper().Trim().Contains(strFilter)).ToListAsync(); 
+                                                 x.Carrera.ToUpper().Trim().Contains(strFilter)).ToListAsync();
             }
             else
                 lst = await db.Estudiante.ToListAsync();
-             
+
 
             return Request.CreateResponse(HttpStatusCode.OK, new Result<List<Estudiante>>()
             {
@@ -45,83 +41,119 @@ namespace LibraryApp.API.Controllers
             });
         }
 
-        // GET: api/Students/5
         [ResponseType(typeof(Estudiante))]
-        public async Task<IHttpActionResult> GetEstudiante(int id)
+        public async Task<HttpResponseMessage> GetEstudiante(int id)
         {
-            Estudiante estudiante = await db.Estudiante.FindAsync(id);
+            // AsNoTracking is defined for remove entity state
+            Estudiante estudiante = await db.Estudiante.AsNoTracking().FirstOrDefaultAsync(x => x.IdLector == id);
+
             if (estudiante == null)
             {
-                return NotFound();
+                return Request.CreateResponse(HttpStatusCode.NotFound, new Result<Estudiante>()
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Message = string.Format("No se encontró registro con el id {0}", id)
+                });
             }
 
-            return Ok(estudiante);
+            return Request.CreateResponse(HttpStatusCode.OK, new Result<Estudiante>()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Data = estudiante
+            });
         }
 
         // PUT: api/Students/5
         [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutEstudiante(int id, Estudiante estudiante)
+        public async Task<HttpResponseMessage> PutEstudiante(int id, Estudiante estudiante)
         {
             if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
 
             if (id != estudiante.IdLector)
             {
-                return BadRequest();
-            }
-
-            db.Entry(estudiante).State = EntityState.Modified;
-
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!EstudianteExists(id))
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new BaseResult()
                 {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Message = string.Format("El campo clave de la entidad debe ser igual a {0}", id)
+                });
             }
 
-            return StatusCode(HttpStatusCode.NoContent);
+            if (EstudianteExists(id))
+            {
+            //    Estudiante record = await db.Estudiante.FindAsync(id);
+            //    record.Nombre = estudiante.Nombre;
+            //    record.Direccion = estudiante.Direccion;
+            //    record.CI = estudiante.CI;
+            //    record.Carrera = estudiante.Carrera;
+            //    record.Edad = estudiante.Edad;
+
+                db.Entry(estudiante).State = EntityState.Modified;
+                try
+                {
+                    await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new BaseResult()
+                    {
+                        ExceptionDetail = ex,
+                        StatusCode = HttpStatusCode.BadRequest,
+                        Message = string.Format("El campo clave de la entidad debe ser igual a {0}", id)
+                    });
+                }
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, new BaseResult()
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Message = string.Format("No se encontró registro con el id {0}", id)
+                });
+            }
+
+            return Request.CreateResponse(HttpStatusCode.NoContent);
         }
 
         // POST: api/Students
         [ResponseType(typeof(Estudiante))]
-        public async Task<IHttpActionResult> PostEstudiante(Estudiante estudiante)
+        public async Task<HttpResponseMessage> PostEstudiante(Estudiante estudiante)
         {
             if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
 
             db.Estudiante.Add(estudiante);
             await db.SaveChangesAsync();
 
-            return CreatedAtRoute("DefaultApi", new { id = estudiante.IdLector }, estudiante);
+            return Request.CreateResponse(HttpStatusCode.Created, new Result<Estudiante>()
+            {
+                Data = estudiante,
+                StatusCode = HttpStatusCode.Created,
+                Message = "Registro creado exitosamente"
+            });
         }
 
         // DELETE: api/Students/5
         [ResponseType(typeof(Estudiante))]
-        public async Task<IHttpActionResult> DeleteEstudiante(int id)
+        public async Task<HttpResponseMessage> DeleteEstudiante(int id)
         {
             Estudiante estudiante = await db.Estudiante.FindAsync(id);
             if (estudiante == null)
-            {
-                return NotFound();
-            }
+                return Request.CreateResponse(HttpStatusCode.NotFound, new BaseResult()
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Message = string.Format("No se encontró registro con el id {0}", id)
+                });
 
             db.Estudiante.Remove(estudiante);
             await db.SaveChangesAsync();
 
-            return Ok(estudiante);
+            return Request.CreateResponse(HttpStatusCode.OK, new Result<Estudiante>()
+            {
+                Data = estudiante,
+                Message = "Se eliminó el registro exitosamente",
+                StatusCode = HttpStatusCode.OK
+            });
         }
 
         protected override void Dispose(bool disposing)
