502978c5850fc6ac948efe8432a072fc612bdc75
ftinoco <fernandotinoco_45@hotmail.com>
2019-09-29 23:25:47 -0600

    Add new mapping route with action

--- LibraryApp.API/App_Start/WebApiConfig.cs
+++ LibraryApp.API/App_Start/WebApiConfig.cs
@@ -23,6 +23,11 @@ namespace LibraryApp.API
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiWithAction",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
             // Remove XML serialization
             config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
             // Add json formatting
